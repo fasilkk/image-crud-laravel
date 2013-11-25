89fcf8bbe55cf7b04275d83850fde7cf13863aef
scoumbourdis <scoumbourdisj@gmail.com>
2013-11-25 20:46:06 +0000

    ImageCRUD laravel

---
+++ development/quick-remove.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# WARNING - This is only for a quick local installation. Please do
+# not use this code at production
+rm -rf app/
+rm -rf laravel/
+rm -rf vendor/
---
+++ development/views/layout.blade.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	@yield('js_files')
+	@yield('css_files')
+</head>
+    <body>
+        <h1>Header 1</h1>
+
+        @yield('content')
+    </body>
+</html>
