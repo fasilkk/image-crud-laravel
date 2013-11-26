73c9bbb51434a396661489f8ac2f9edcbf591736
scoumbourdis <scoumbourdisj@gmail.com>
2013-11-26 23:07:29 +0000

    minor changes.

--- development/quick-install.sh
+++ development/quick-install.sh
@@ -2,6 +2,7 @@
 
 # WARNING - This is only for a quick local installation. Please do
 # not use this code at production
+cd ../
 composer install
 composer create-project laravel/laravel --prefer-dist
 mkdir app
@@ -13,7 +14,8 @@ cp development/controllers/* app/controllers
 cp development/config/* app/config
 cp development/views/* app/views
 cp development/composer.json laravel/composer.json
-cd laravel && composer update
+cd laravel
+composer update
 cd ../
 mkdir laravel/public/assets && mkdir laravel/public/assets/image_crud
 cp -rf assets/image_crud/public/* laravel/public/assets/image_crud/
