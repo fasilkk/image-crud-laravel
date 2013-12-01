7b20d71affc43ce676eb55d02fdd88facf74c81e
scoumbourdis <scoumbourdisj@gmail.com>
2013-12-01 12:08:16 +0000

    Adding linkable example at the layout
    Adding linkable example at the layout + .gitignore update

--- development/views/layout.blade.php
+++ development/views/layout.blade.php
@@ -1,11 +1,17 @@
 <html>
 <head>
-	@yield('js_files')
-	@yield('css_files')
+    @yield('js_files')
+    @yield('css_files')
 </head>
-    <body>
-        <h1>Header 1</h1>
-
-        @yield('content')
-    </body>
+<body>
+    <div>
+        <a href='<?php echo URL::to('/example/example1'); ?>'>Example 1 - Simple</a> |
+        <a href='<?php echo URL::to('/example/example2'); ?>'>Example 2 - With ordering</a> |
+        <a href='<?php echo URL::to('/example/example3/22'); ?>'>Example 3 - With category id</a> |
+        <a href='<?php echo URL::to('/example/example4'); ?>'>Example 4 - Images with editable title</a> |
+        <a href='<?php echo URL::to('/example/example5'); ?>'>Example 5 - Photo Gallery without uploader</a>
+    </div>
+    <br/><br/>
+    @yield('content')
+</body>
 </html>
